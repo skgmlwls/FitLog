565fa0416d34d8342e550ff55508327772aa16f1
skgmlwls <127082621+skgmlwls@users.noreply.github.com>
2025-07-23 03:04:00 +0900

    작ìstart

--- app/src/main/java/com/nhj/fitlog/FitLogApplication.kt
+++ app/src/main/java/com/nhj/fitlog/FitLogApplication.kt
@@ -1,10 +1,14 @@
 package com.nhj.fitlog
 
 import android.app.Application
+import androidx.navigation.NavHostController
 import dagger.hilt.android.HiltAndroidApp
 
 // Hilt를 사용하려면 이 어노테이션을 애플리케이션의 Application 클래스에 선언해야 함
 @HiltAndroidApp
 class FitLogApplication : Application() {
+
+    // 네비게이션
+    lateinit var navHostController: NavHostController
 
 }
